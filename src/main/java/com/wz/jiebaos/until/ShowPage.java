60bb692db913f07eb372cc6e61ab04054e1598de
jiejie <jie_1004@qq.com>
2023-03-22 14:40:15 +0800

    hot-fix  commit1

--- src/main/java/com/wz/jiebaos/until/ShowPage.java
+++ src/main/java/com/wz/jiebaos/until/ShowPage.java
@@ -3,4 +3,9 @@ package com.wz.jiebaos.until;
 //分页工具类
 public class ShowPage {
     public static final int PAGE_SIZE = 10;
+
+    public void GitTest()
+    {
+        System.out.println("hello git");
+    }
 }
